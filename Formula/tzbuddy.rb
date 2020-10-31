30a24356011fe42a6d8199e6520798a92fc83372
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-31 14:43:56 +0000

    Fix version/hash

--- Formula/tzbuddy.rb
+++ Formula/tzbuddy.rb
@@ -2,8 +2,8 @@ class Tzbuddy < Formula
   desc "Simple cli to visualize times in different timezones."
   homepage "https://github.com/gbagnoli/tzbuddy.rs"
   url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.1.7/tzbuddy-macos-amd64.tar.gz"
-  sha256 "713201487877079cad78b1a3361ea7fb21f84d5ef394913bdfdd747b1b56af50"
-  version "0.1.6"
+  sha256 "1714cb360e933b796bbfdf49e67f6fb6dace7b8f8461d3f3b161e82d5d13d4d7"
+  version "0.1.7"
 
   def install
     bin.install "tzbuddy"
