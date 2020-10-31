4f2ff2fd903a1a46665b06424f6e08b731968b01
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-31 14:36:01 +0000

    bump down

--- Formula/tzbuddy.rb
+++ Formula/tzbuddy.rb
@@ -3,7 +3,7 @@ class Tzbuddy < Formula
   homepage "https://github.com/gbagnoli/tzbuddy.rs"
   url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.1.7/tzbuddy-macos-amd64.tar.gz"
   sha256 "713201487877079cad78b1a3361ea7fb21f84d5ef394913bdfdd747b1b56af50"
-  version "0.1.7"
+  version "0.1.6"
 
   def install
     bin.install "tzbuddy"
