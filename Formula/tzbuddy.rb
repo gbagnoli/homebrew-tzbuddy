50cce5a88c771e76bd84a31d3075e8132bd2fd28
Giacomo Bagnoli <gbagnoli@gmail.com>
2021-02-21 17:28:32 +0000

    tzbuddy 0.1.10
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tzbuddy.rb
+++ Formula/tzbuddy.rb
@@ -1,9 +1,9 @@
 class Tzbuddy < Formula
   desc "Simple cli to visualize times in different timezones."
   homepage "https://github.com/gbagnoli/tzbuddy.rs"
-  url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.1.9/tzbuddy-macos-amd64.tar.gz"
-  sha256 "9aad20ee9dd8a37859c6da5d0b8e00eae1ca57e042555eefd5b5b2d8ca4446f0"
-  version "0.1.9"
+  url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.1.10/tzbuddy-macos-amd64.tar.gz"
+  sha256 "393a09c238768bcec0ec12dab1223a9ee42f8ae78051696f8a92069e1db94616"
+  version "0.1.10"
 
   def install
     bin.install "tzbuddy"
