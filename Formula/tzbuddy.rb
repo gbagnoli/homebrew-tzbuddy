72dc103ff988fe4340634942fae4979d7f25e86a
Giacomo Bagnoli <gbagnoli@gmail.com>
2022-03-03 23:33:13 +0000

    Release 0.2.0

--- Formula/tzbuddy.rb
+++ Formula/tzbuddy.rb
@@ -1,9 +1,9 @@
 class Tzbuddy < Formula
   desc "Simple cli to visualize times in different timezones."
   homepage "https://github.com/gbagnoli/tzbuddy.rs"
-  url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.1.10/tzbuddy-macos-amd64.tar.gz"
-  sha256 "393a09c238768bcec0ec12dab1223a9ee42f8ae78051696f8a92069e1db94616"
-  version "0.1.10"
+  url "https://github.com/gbagnoli/tzbuddy.rs/releases/download/v0.2.0/tzbuddy-macos-amd64.tar.gz"
+  sha256 "313d3c91b6250fbbb813c931e9faea6a355fc61b5d12b882d477b2f5aa8bcc32"
+  version "0.2.0"
 
   def install
     bin.install "tzbuddy"
